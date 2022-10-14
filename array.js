2bcdb56155414d0f2ed31133d1a78c77780fdfc6
Nick Kandelaki <goodo.nick@gmail.com>
2022-10-14 17:46:56 +0400

    jsArray

---
+++ array.js
@@ -0,0 +1,27 @@
+
+//Массив с 5ю машинами//
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+
+
+//Добавление 2х элементов в конце массива с помощью push()//
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+myArray.push('kia','skoda')
+
+
+//Удаление последнего элемента массива с помощью pop()/
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+myArray.pop() //удалит последний элемент массива, то есть mini//
+
+//Добавление нового элемента в начале массива с помощью unshift()//
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+myArray.unshift('kia')
+
+// Удаление элемента в начале массива, с помощью shift()//
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+myArray.shift()
+
+//Подсчет длины массива с помощью length//
+const myArray = ['volvo', 'audi', 'bmw', 'mersedes', 'mini']
+myArray.length
+
+
